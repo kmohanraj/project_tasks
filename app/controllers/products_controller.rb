c77491a1b88d7a9c185c00558487cb9b8cb2b915
kmohanraj <mohanksv.89@gmail.com>
2017-12-19 23:56:06 +0530

    products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,15 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!
+  # before_filter :authenticate_user!
   before_action :set_new_product, only: [:index, :new, :create]
   require 'will_paginate/array'
 
   def index
-    products = Product.recent_project if params[:order] == "recent_project"
-    products = Product.order_project_title if params[:order] == "order_project_title"
-    products = Product.order_user_name if params[:order] == "order_user_name"
-    products = Product.order_category_name if params[:order] == "order_category_name"
-    products = Product.includes(:category).order( 'created_at DESC' )
+    products = Product.order(created_at: :asc) if params[:order] == "latest"
+    products = Product.order(name: :asc) if params[:order] == "project_title"
+    products = Product.order(user_id: :asc) if params[:order] == "user_name"
+    products = Product.order(category_id: :asc) if params[:order] == "category_name"
    # @products = products.paginate(page: params[:page]).order("created_at DESC").per_page(2)
-    @products = products.paginate(page: params[:page]).per_page(2)
+    @products = products.page(params[:page]).per_page(3)
     respond_to do |format|
       format.json {render json: @products, status: :ok }
       format.js
@@ -23,7 +22,7 @@ class ProductsController < ApplicationController
     respond_to do |format|
       if @product_created.save
         format.json {render json: @product_created, status: :created }
-        format.js 
+        format.js
         format.html {redirect_to :controller=> "products", :action => "index", flash: { success: "Product saved Successfully"}}
       else
         format.json {render json: @product_created.errors.full_messages, status: :unprocessable_entity }
@@ -34,7 +33,7 @@ class ProductsController < ApplicationController
   end
 
   private
-	
+
   def product_params
 		params.require(:product).permit(:user_id, :category_id, :name, :price)
 	end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,11 +4,7 @@ class Product < ApplicationRecord
   belongs_to :category
   
   before_validation :set_product_date, on: :create
-  scope :recent_project, -> { order("created_at DESC")}
-  scope :order_project_title, -> { order("name ASC")}
-  scope :order_category_name, -> { order(category_id: :asc)}
-  scope :order_user_name, -> { order("user_id ASC")}
-
+  
   def set_product_date
 	self.date = Date.today if self.date.blank?
   end
